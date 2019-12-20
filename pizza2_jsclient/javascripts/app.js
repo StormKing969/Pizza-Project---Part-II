8f4e4e54f389becac02f0d95a7dcbc195e1f1489
StormKing969 <wijesinghesaj@gmail.com>
2019-12-20 12:37:23 -0500

    Add files via upload

--- pizza2_jsclient/javascripts/app.js
+++ pizza2_jsclient/javascripts/app.js
@@ -2,7 +2,7 @@
 // avoid warnings on using fetch and Promise --
 /* global fetch, Promise */
 // use port 80, i.e., apache server for webservice execution
-const baseUrl = "http://localhost/cs637/wasajana/pizza2_server/api";
+const baseUrl = "http://topcat.cs.umb.edu/cs637/wasajana/pizza2_server/api";
 // globals representing state of data and UI
 let selectedUser = 'none';
 let sizes = [];
@@ -31,8 +31,8 @@ function displaySizesToppingsOnHomeTab() {
     for (var i = 0; i < sizes.length; i++) {
         var size = document.getElementById("sizes");
         var x = sizes[i].size;
-        size.appendChild(document.createTextNode(x)); 
-        size.appendChild (document.createTextNode("\u00A0\u00A0\u00A0"));   
+        size.appendChild(document.createTextNode(x));
+        size.appendChild (document.createTextNode("\u00A0\u00A0\u00A0"));  
     }
     for (var i = 0; i < toppings.length; i++) {
         var topp = document.getElementById("toppings");
@@ -52,14 +52,30 @@ function setupUserForm() {
     //  Also change the visibility of the order-area
     // and redisplay the orders
 
-    var x = document.getElementById("userselect");
-    for (var i = 0; i < users.length; i++) {
-        var hor = document.createElement("option");
-        hor.text = users[i].username;
-        // hor.appendChild(document.createTextNode(x));
-        x.add(hor);
-    }
+    $('#userselect').append(users.map((user)=>{
+        console.log(user);
+        return $('<option />').val(user.id).text(user.username);
+    }))
+    // var x = document.getElementById("userselect");
+    // for (var i = 0; i < users.length; i++) {
+    //     var hor = document.createElement("option");
+    //     hor.text = users[i].username;
+    //     // hor.appendChild(document.createTextNode(x));
+    //     x.add(hor);
+    
+
+    // Jquery
+    $('#userselectsubmit').on("click", function (event){
+      event.preventDefault();
+      let userid = $('#userselect').val();
+      let selectedUser = $('#userselect option:selected').text();
+      $("#username-fillin1, #username-fillin2").html(selectedUser);
+      $("#order-area").addClass("active");
+      displayOrders(userid);
+    });
+    // alert(selectedOption);
 }
+
 function setupAcknowledgeForm() {
     console.log("setupAckForm...");
     document.querySelector("#ackform input").addEventListener("click", function () {
@@ -72,35 +88,43 @@ function setupAcknowledgeForm() {
                 updateOrder(order); // post update to server
             }
         });
-        displayOrders(orders);
+        displayOrders();
         return false;
     });
 }
+
 function setupRefreshOrderListForm() {
     console.log("setupRefreshForm...");
     document.querySelector("#refreshbutton input").addEventListener("click", function () {
         //  $("#refreshbutton input").on("click", function () {
         console.log("refresh orders by user = " + selectedUser);
-        getOrders();
         return false;
     });
 }
-function displayOrders() {
-    console.log("displayOrders");
 
-    // remove class "active" from the order-area
-    // if selectedUser is "none", just return--nothing to do
-    // empty the ordertable, i.e., remove its content: we'll rebuild it
-    // add class active to order-area
-    // find the user_id of selectedUser via the users array
-    // find the in-progress orders for the user by filtering array
-    // orders on user_id and status
-    // if there are no orders for user, make ordermessage be "none yet"
-    //  and remove active from element id'd order-info
-    // Otherwise, add class active to element order-info, make
-    //   ordermessage be "", and rebuild the order table
-    // Finally, if there are Baked orders here, make sure that
-    // ackform is active, else not active
+function displayOrders(userid=false) {
+    // Jquery
+    getOrders().then((orders)=>{
+        $('table.test').remove();
+        var theadrow = $('<tr>').append('<td>Order Id</td>')
+                                .append('<td>Size</td>')
+                                .append('<td>Toppings</td>')
+                                .append('<td>Status</td>');
+        var thead = $('<thead />').append(theadrow)
+        var tbody = $('<tbody />');
+        orders.map(o=>{
+            console.log(o,userid);
+            if (o.user_id === userid) {
+                var tbodyrow = $('<tr>').append('<td>' + o.id + '</td>')
+                                        .append('<td>' + o.size + '</td>')
+                                        .append('<td>' + o.toppings + '</td>')
+                                        .append('<td>' + o.status + '</td>');
+                tbody.append(tbodyrow);
+            }
+        })
+        $('<table class="test" />').append(thead).append(tbody).appendTo('.tabContent.home');
+    })
+    
 }
 
 // Let user click on one of two tabs, show its related contents
@@ -111,23 +135,13 @@ function displayOrders() {
 // content nearby. The related content needs to be a sibling of
 // the clicked-on element's grandparent.
 function setupTabs() {
-    console.log("starting setupTabs");
-
-    // Do this last. You may have a better approach, but here's one
-    // way to do it. Also edit the html for better initial settings
-    // of class active on these elements.
-    // Find <span> elements inside <a>'s inside elements with class tabs
-    // and process them as follows:  (there are four of them)
-    // add a click listener to the element. When a click happens,
-    // add class "active" to that element, and figure out this element's
-    // parent's (the parent is an <a>) position among its siblings. If it
-    // is the first child, the other <a> is its next sibling, and the other
-    // <span> is the first child of that <a>. Similarly in the other case.
-    // Remove class active from that other tab.
-    // Now find the related tabContent element. It's the <span>'s
-    // grandparent's next sibling, or sibling after that. Add class active
-    // to the newly active one and remove it from the other one.
-
+    // Jquery
+    $('.tabs a span').on('click',function (e) {
+        e.preventDefault();
+        $('.tabContent.active,.tabs a span').removeClass('active');
+        $(e.target).addClass('active');
+        $('.tabContent.' + $(e.target).data('type')).addClass('active');
+    });
 }
 
 function displaySizesToppingsOnOrderForm() {
@@ -138,78 +152,80 @@ function displaySizesToppingsOnOrderForm() {
     // Then find the spot for meat toppings, and meatless ones
     // and for each create an <input> element for a checkbox
     // and a <label> for each
+    // var label = document.getElementById("order-sizes");
     for (var i = 0; i < sizes.length; i++) {
         var size = document.getElementById("order-sizes");
+        var radio = document.createElement("input");
         var x = sizes[i].size;
+        radio.type = "radio";
+        radio.name ='size';
+        radio.value = x;
+        size.appendChild(radio);
         size.appendChild(document.createTextNode(x + '\u00A0\u00A0\u00A0'));    
     }
 
+    for (let i = toppings.length - 1; i >= 0; i--) {
+        var topp_withmeat = document.getElementById("meats");
+        var checkbox = document.createElement("input");
+        var type = toppings[i].is_meat;
+        if (type == 1) {
+            var topping_meat = toppings[i].topping;
+            checkbox.type = "checkbox";
+            checkbox.name ='order_topping';
+            checkbox.value = topping_meat;
+            topp_withmeat.appendChild(checkbox);
+            topp_withmeat.appendChild(document.createTextNode(topping_meat + '\u00A0\u00A0\u00A0'));
+            // topp.content.innerHTML += "<li>" + x + "</li>";
+        }     
+    }
 
-    // for (var i = 0; i < toppings.length; i++) {
-    //     var topp = document.getElementById("meats");
-    //     var type = toppings[i].is_meat
-    //     if (type == 1) {
-    //         var x = toppings[i].topping;
-    //         topp.appendChild(document.createTextNode(x + '             '));
-    //     }    
-    // }
+    for (let i = toppings.length - 1; i >= 0; i--) {
+        var topp_withoutmeat = document.getElementById("meatlesses");
+        var checkbox = document.createElement("input");
+        var type = toppings[i].is_meat;
+        if (type == 0) {
+            var topping_meatless = toppings[i].topping;
+            checkbox.type = "checkbox";
+            checkbox.name ='order_topping';
+            checkbox.value = topping_meatless;
+            topp_withoutmeat.appendChild(checkbox);
+            topp_withoutmeat.appendChild(document.createTextNode(topping_meatless + '\u00A0\u00A0\u00A0'));
+            // topp.content.innerHTML += "<li>" + x + "</li>";
+        }     
+    }
 
-    // for (var i = 0; i < toppings.length; i++) {
-    //     var topp = document.getElementById("meatlesses");
-    //     var type = toppings[i].is_meat
-    //     if (type == 0) {
-    //         var x = toppings[i].topping;
-    //         topp.appendChild(document.createTextNode(x + '             '));
-    //     }    
-    // }
+    document.getElementById("meats").classList.remove("active");
+    document.getElementById("meatlesses").classList.remove("active");
 
-    document.querySelectorAll(".tabs.meat-meatless a span").forEach(function (element) {
+    document.querySelectorAll(".topping-tabs.meat-meatless a .active").forEach(function (element) {
     // Code for page 139-140, to finish Chap. 4 Amazeriffic:
         element.addEventListener("click", function () {
             event.preventDefault();
-            document.querySelectorAll(".tabs.meat-meatless a span").forEach(function (element) {
+            document.querySelectorAll(".topping-tabs.meat-meatless a .active").forEach(function (element) {
                 element.classList.remove("active");
             });
-            element.classList.add("active");
-            document.querySelector("#meats").innerHTML = "";
-            console.log("TAB clicked: " + element.innerHTML);
-            let content;
+            // element.classList.add("active");
+            // document.querySelector("#meats").innerHTML = "";
+            // console.log("TAB clicked: " + element.innerHTML);
+            // let content;
             if (element.parentElement.matches(":nth-child(1)")) {
-                console.log("in newest");
-                // newest first, so we have to go through
-                // the array backwards
-                content = document.createElement("ul");
-                for (let i = toppings.length - 1; i >= 0; i--) {
-                    var topp = document.getElementById("meats");
-                    var type = toppings[i].is_meat
-                    if (type == 1) {
-                        var x = toppings[i].topping;
-                        content.innerHTML += "<li>" + x + "</li>";
-                    }   
-                }
-
+                document.getElementById("meats").classList.add("active");
+                document.getElementById("meatlesses").classList.remove("active");
             } else
             if (element.parentElement.matches(":nth-child(2)")) {
-                content = document.createElement("ul");
-                for (let i = toppings.length - 1; i >= 0; i--) {
-                    var topp = document.getElementById("meatlesses");
-                    var type = toppings[i].is_meat
-                    if (type == 0) {
-                        var x = toppings[i].topping;
-                        content.innerHTML += "<li>" + x + "</li>";
-                    }   
-                }
+                document.getElementById("meats").classList.remove("active");
+                document.getElementById("meatlesses").classList.add("active"); 
             }
-            document.querySelector("#meats").append(content);
+            // document.querySelector("#meats").append(content);
         });
     });
     // simulate a click to fill in first-child content
     // document.querySelector(".tabs a:first-child span").click(); // insta refresh
+
 }
 
 function setupOrderForm() {
     console.log("setupOrderForm...");
-
     // find the orderform's submitbutton and put an event listener on it
     // When the click event comes in, figure out the sizeName from
     // the radio button and the toppings from the checkboxes
@@ -218,10 +234,25 @@ function setupOrderForm() {
     // compose an order, and post it. On success, report the
     // new order number to the user using order-message
 
+    let orderButton = document.querySelector(".submitbutton").addEventListener("click", function(event) {
+        var chosen_size = document.querySelector('input[name="size"]:checked').value;
+        console.log(chosen_size);
+        var chosen_toppings = document.querySelectorAll('input[name="order_topping"]:checked').value;
+        console.log(chosen_toppings);
+        var currentUser = document.getElementById("username-fillin2").innerHTML;
+        console.log(currentUser);
+        var userorder = {};
+        userorder.user_id = currentUser;
+        userorder.size = chosen_size;
+        userorder.day = "1";
+        userorder.status = "Preparing";
+        // userorder.toppings = ["toppings_chosen"];
+        postOrder(userorder);
+    });
 }
 
 // Plain modern JS: use fetch, which returns a "promise"
-// that we can combine with other promises and wait for all to finish
+// that we can combine with other promises and wait for all to finish 
 function getSizes() {
     let promise = fetch(
             baseUrl + "/sizes",
@@ -235,6 +266,7 @@ function getSizes() {
             .catch(error => console.error('error in getSizes:', error));
     return promise;
 }
+
 // JQuery/Ajax: for use with $.when: return $.ajax object
 function getSizes0() {
     return $.ajax({
@@ -259,6 +291,7 @@ function getToppings() {
             .then(json => {
                 console.log("back from fetch: %O", json);
                 toppings = json;
+                console.log(toppings);
             })
             .catch(error => console.error('error in getToppings:', error));
     return promise;
@@ -293,14 +326,74 @@ let promise = fetch(
 }
 
 function getOrders() {
-
+   let promise = new Promise((resolve,reject)=>{
+        fetch(baseUrl + "/orders")
+            .then(response => resolve(response.json()))
+            .catch((e)=>reject(e))
+    });
+   console.log(promise);
+    return promise;
 }
+
+    // return [{
+    //         "id":5,
+    //         "toppings": ['vegan cheese','mushrooms'],
+    //         "size": "XXXXL",
+    //         "status": "Prepaired"
+    //     },{
+    //         "orderId":6,
+    //         "toppings": ['ananas','banana'],
+    //         "size": "s",
+    //         "status": "Wait to be acceted"
+    //     },{
+    //         "orderId":7,
+    //         "toppings": ['chilli','zuccini'],
+    //         "size": "M",
+    //         "status": "Out for delivery"
+    //     },{
+    //         "orderId":2,
+    //         "toppings": ['seewead','eggplant'],
+    //         "size": "XL",
+    //         "status": "Delivered"
+    //     }]
+// }
 function updateOrder(order) {
 
 }
-function postOrder(order, onSuccess) {
 
+function postOrder(order) {
+
+    let promise = fetch(
+            baseUrl + "/orders",
+            {method: 'post',
+            headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify(order)
+   })
+            .then(response => response.json())
+            // .then(json => {
+            //     console.log("back from fetch: %O", json);
+                
+            // }
+            .catch(error => console.error('error in getUsers:', error));
+    return promise;
 }
+
+    // return $.ajax({
+    //     url: baseUrl + "/orders",
+    //     type: "POST",
+    //     dataType: "json",
+    //     //  headers: {"Content-type":"application/json"}, // needed
+    //     success: function (result) {
+    //         console.log("We did POST to /orders");
+    //         console.log(result);
+    //         orders = result;
+    //     }
+    // });
+// }
+
 function refreshData(thenFn) {
     // wait until all promises from fetches "resolve", i.e., finish fetching
     Promise.all([getSizes(), getToppings(), getUsers(), getOrders()]).then(thenFn);
